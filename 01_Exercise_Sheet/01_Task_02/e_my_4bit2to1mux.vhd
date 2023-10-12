8a828e0496098ea3fe679c3215fd742c77ceee5e
FAZELI SHAHROUDI Sepehr (INTERN) <sepehr.fazelishahroudi.intern@3ds.com>
2023-10-12 22:16:19 +0200

    Ex1 T2 final

---
+++ 01_Exercise_Sheet/01_Task_02/e_my_4bit2to1mux.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity e_my_4bit2to1mux is
+    port (
+        sw    : in    std_logic_vector (9 downto 0);
+        LEDR  : out   std_logic_vector (9 downto 0)
+    );
+end entity e_my_4bit2to1mux;
+
+architecture a_my_4bit2to1mux_1 of e_my_4bit2to1mux is
+    signal s : std_logic; -- Declare s as a signal
+    signal X : std_logic_vector(3 downto 0); -- Declare X as a signal
+    signal Y : std_logic_vector(3 downto 0); -- Declare Y as a signal
+
+begin
+    process(sw)
+    begin
+        s <= sw(9); -- Assign sw(9) to signal s
+        X <= sw(3 downto 0); -- Assign SW[3-0] to signal X
+        Y <= sw(7 downto 4); -- Assign SW[7-4] to signal Y
+        -- Use s as the address input
+        -- Use X as input vector X
+        -- Use Y as input vector Y
+        -- Output M is selected based on the value of the address input
+        LEDR(9) <= s;
+        LEDR(3 downto 0) <= (others => '0'); -- Initialize LEDR[3-0] to 0
+
+        if s = '0' then
+            LEDR(3 downto 0) <= X; -- Select X
+        else
+            LEDR(3 downto 0) <= Y; -- Select Y
+        end if;
+    end process;
+end architecture a_my_4bit2to1mux_1;
---
+++ 01_Exercise_Sheet/01_Task_02/output_files/e_my_4bit2to1mux.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 22.1std.1 Build 917 02/14/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("E:/intelFPGA_lite/FPGA_Excercises/FPGA-programming/01_Exercise_Sheet/01_Task_02/output_files/") File("e_my_4bit2to1mux.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
