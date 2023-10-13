cbb0da566eaf790a9f8ad1048d9220b9d2441623
Sepehr Fazeli <59790250+sepehrfazeli@users.noreply.github.com>
2023-10-13 11:03:07 +0200

    Update Ex1 T2 with class

--- 01_Exercise_Sheet/01_Task_02/e_my_4bit2to1mux.vhd
+++ 01_Exercise_Sheet/01_Task_02/e_my_4bit2to1mux.vhd
@@ -2,33 +2,35 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity e_my_4bit2to1mux is
-    port (
-        sw    : in    std_logic_vector (9 downto 0);
-        LEDR  : out   std_logic_vector (9 downto 0)
-    );
+port (
+SW : in std_logic_vector(9 downto 0);
+LEDR : out std_logic_vector(9 downto 0));
 end entity e_my_4bit2to1mux;
 
-architecture a_my_4bit2to1mux_1 of e_my_4bit2to1mux is
-    signal s : std_logic; -- Declare s as a signal
-    signal X,Y : std_logic_vector(3 downto 0); -- Declare X, M as a signal
+architecture a_my_4bit2to1mux of e_my_4bit2to1mux is
+
+-- Declaration Part
+
+-- Signal declaration
+
+signal s1_S_int : std_logic;
+signal s1v_X_int, s1v_Y_int, slv_M_int : std_logic_vector(3 downto 0);
 
 begin
-    process(sw)
-    begin
-        s <= sw(9); -- Assign sw(9) to signal s
-        X <= sw(3 downto 0); -- Assign SW[3-0] to signal X
-        Y <= sw(7 downto 4); -- Assign SW[7-4] to signal Y
-        -- Use s as the address input
-        -- Use X as input vector X
-        -- Use Y as input vector Y
-        -- Output M is selected based on the value of the address input
-        LEDR(9) <= s;
-        LEDR(3 downto 0) <= (others => '0'); -- Initialize LEDR[3-0] to 0
 
-        if s = '0' then
-            LEDR(3 downto 0) <= X; -- Select X
-        else
-            LEDR(3 downto 0) <= Y; -- Select Y
-        end if;
-    end process;
-end architecture a_my_4bit2to1mux_1;
+s1v_X_int <= SW(3 downto 0);
+s1v_Y_int <= SW(7 downto 4);
+s1_S_int <= SW(9);
+
+
+slv_M_int: for j in 0 to 3 generate
+slv_M_int(j) <= (not(s1_S_int) and s1v_X_int(j)) or (s1_S_int and s1v_Y_int(j));
+end generate;
+
+
+LEDR(9) <= s1_S_int;
+LEDR(8 downto 4) <= "00000";
+LEDR(3 downto 0) <= slv_M_int;
+
+
+end architecture a_my_4bit2to1mux;
